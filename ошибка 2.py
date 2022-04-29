2361350e51d2434fe5ce2331cd5ee18858d4dbc6
youtude93 <89247947+youtude93@users.noreply.github.com>
2022-04-29 16:55:34 +0300

    Add files via upload

---
+++ ошибка 2.py
@@ -0,0 +1,9 @@
+try:
+    a = int(input("Enter first number: "))
+    b = int(input("Enter second number: "))
+    print(a*b)
+    print("Success")
+except:
+    print("some bug in user input!!!")
+
+print("This line always execute...")
